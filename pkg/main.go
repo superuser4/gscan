091db17be5f65a39a70340280801c369d0a4163a
superuser4 <superuser3344@protonmail.com>
2025-05-11 14:42:53 -0400

    basic port scanning works

---
+++ pkg/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func ScanTarget(IP string, Ports int) {
+	for port := 1; port <= Ports; port++ {
+		func(p int) {
+			conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", IP, p), 500*time.Millisecond)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d	open\n", p)
+		}(port)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No Target IP provided! Quitting...")
+		os.Exit(1)
+	}
+	var IPaddr string = os.Args[1]
+	log.Println("GScan Scanning Target:", IPaddr)
+	fmt.Println("PORT	STATE	SERVICE")
+	ScanTarget(IPaddr, 30000)
+}
