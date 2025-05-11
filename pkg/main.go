f553a7b0dfd8a848f0e8185682a0c8870fed6349
superuser4 <superuser3344@protonmail.com>
2025-05-11 15:46:37 -0400

    added make.sh

--- pkg/main.go
+++ pkg/main.go
@@ -1,41 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"sort"
 	"sync"
 	"time"
 )
 
 func ScanTarget(IP string, Ports int) {
-	var wg sync.WaitGroup
+	var (
+		wg        sync.WaitGroup
+		sem       = make(chan struct{}, 1000)
+		mu        sync.Mutex
+		openPorts []int
+	)
 
 	for port := 1; port <= Ports; port++ {
 		wg.Add(1)
+		sem <- struct{}{}
 
 		go func(p int) {
 			defer wg.Done()
+			defer func() { <-sem }()
 
-			conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", IP, p), 500*time.Millisecond)
+			addr := fmt.Sprintf("%s:%d", IP, p)
+			conn, err := net.DialTimeout("tcp", addr, 500*time.Millisecond)
 			if err != nil {
 				return
 			}
 			conn.Close()
-			fmt.Printf("%d	open\n", p)
+			mu.Lock()
+			openPorts = append(openPorts, p)
+			mu.Unlock()
 		}(port)
 	}
 	wg.Wait()
+
+	sort.Ints(openPorts)
+	for i := 0; i < len(openPorts); i++ {
+		fmt.Printf("%d	%s\n", openPorts[i], "open")
+	}
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("No Target IP provided! Quitting...")
+	var (
+		IPaddr    string
+		portRange int
+	)
+
+	flag.StringVar(&IPaddr, "ip", "127.0.0.1", "Target IP address")
+	flag.IntVar(&portRange, "ports", 1000, "Target Port Range")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		flag.Usage()
 		os.Exit(1)
 	}
-	var IPaddr string = os.Args[1]
+
+	if IPaddr == "" {
+		fmt.Fprintln(os.Stderr, "Error: --ip is required.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	log.Println("GScan Scanning Target:", IPaddr)
 	fmt.Println("PORT	STATE	SERVICE")
-	ScanTarget(IPaddr, 30000)
+	ScanTarget(IPaddr, portRange)
 }
