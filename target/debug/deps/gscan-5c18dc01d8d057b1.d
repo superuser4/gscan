6ba0441fd2eb85afeb30d2c1ff5fd8967ca54a27
superuser4 <superuser3344@protonmail.com>
2025-05-31 20:35:38 -0400

    inital code

--- target/debug/deps/gscan-5c18dc01d8d057b1.d
+++
@@ -1,6 +0,0 @@
-/home/superuser/Sync/docs/programming/gscan/target/debug/deps/gscan-5c18dc01d8d057b1: src/main.rs src/scan.rs
-
-/home/superuser/Sync/docs/programming/gscan/target/debug/deps/gscan-5c18dc01d8d057b1.d: src/main.rs src/scan.rs
-
-src/main.rs:
-src/scan.rs:
--- target/debug/gscan.d
+++
@@ -1,1 +0,0 @@
-/home/superuser/Sync/docs/programming/gscan/target/debug/gscan: /home/superuser/Sync/docs/programming/gscan/src/main.rs /home/superuser/Sync/docs/programming/gscan/src/scan.rs
