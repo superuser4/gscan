30e210d798139d582fac38c8ddea7825d77f1e5f
superuser4 <superuser3344@protonmail.com>
2025-05-31 20:41:45 -0400

    shortened

--- src/scan.rs
+++ src/scan.rs
@@ -2,9 +2,5 @@ use std::net::TcpStream;
 
 pub fn scan_port(ip_addr: String, port: u16) -> bool {
     let ip_and_port: String = ip_addr + ":" + &port.to_string();
-    if let Ok(_stream) = TcpStream::connect(ip_and_port) {
-        true
-    } else { 
-        false
-    }
+    TcpStream::connect(ip_and_port).is_ok()
 }
